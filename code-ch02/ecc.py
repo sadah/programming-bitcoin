71bc62988fd0bce466ffc83a751b28cc0fd74e03
sadah <sadaaki.hirai@gmail.com>
2022-02-05 19:03:55 +0900

    Do chapter2

--- code-ch02/ecc.py
+++ code-ch02/ecc.py
@@ -141,8 +141,7 @@ class Point:
     # end::source1[]
 
     def __ne__(self, other):
-        # this should be the inverse of the == operator
-        raise NotImplementedError
+        return not self.__class__.__eq__(self, other)
 
     def __repr__(self):
         if self.x is None:
@@ -154,7 +153,7 @@ class Point:
     def __add__(self, other):  # <2>
         if self.a != other.a or self.b != other.b:
             raise TypeError('Points {}, {} are not on the same curve'.format
-            (self, other))
+                            (self, other))
 
         if self.x is None:  # <3>
             return other
@@ -164,21 +163,30 @@ class Point:
 
         # Case 1: self.x == other.x, self.y != other.y
         # Result is point at infinity
+        if self.x == other.x and self.y != other.y:
+            return self.__class__(None, None, self.a, self.b)
 
         # Case 2: self.x ≠ other.x
         # Formula (x3,y3)==(x1,y1)+(x2,y2)
         # s=(y2-y1)/(x2-x1)
         # x3=s**2-x1-x2
         # y3=s*(x1-x3)-y1
+        if self.x != other.x:
+            s = (other.y - self.y) / (other.x - self.x)
+            x3 = s ** 2 - self.x - other.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
 
         # Case 3: self == other
         # Formula (x3,y3)=(x1,y1)+(x1,y1)
         # s=(3*x1**2+a)/(2*y1)
         # x3=s**2-2*x1
         # y3=s*(x1-x3)-y1
-
-        raise NotImplementedError
-
+        if self == other:
+            s = (3 * self.x**2 + self.a) / (2 * self.y)
+            x3 = s ** 2 - 2 * self.x
+            y3 = s * (self.x - x3) - self.y
+            return self.__class__(x3, y3, self.a, self.b)
 
 class PointTest(TestCase):
 
